f1e01b82b262bbb41e9451ac567f16df5bb5d5cd
Nguyen Le <69196079+lecnguyenn@users.noreply.github.com>
2020-11-07 14:09:04 +0700

    Create Maincomponent.js

---
+++ component/Maincomponent.js
@@ -0,0 +1,13 @@
+. . .
+
+import { actions } from 'react-redux-form';
+
+. . .
+
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
+  
+. . .
+
+              <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+              
+. . .
