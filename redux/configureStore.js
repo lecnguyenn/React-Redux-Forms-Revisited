1941daceb9f8dcca00a90c76390be1e44cd482af
Nguyen Le <69196079+lecnguyenn@users.noreply.github.com>
2020-11-07 14:07:41 +0700

    Create configureStore.js

---
+++ redux/configureStore.js
@@ -0,0 +1,21 @@
+. . .
+
+import { createForms } from 'react-redux-form';
+
+. . .
+
+import { InitialFeedback } from './forms';
+
+. . .
+
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
+        }),
+        
+. . .
