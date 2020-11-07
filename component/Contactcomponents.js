562b5af4a7eb864e413f7dde10875db76b06dd1d
Nguyen Le <69196079+lecnguyenn@users.noreply.github.com>
2020-11-07 14:09:49 +0700

    Create Contactcomponents.js

---
+++ component/Contactcomponents.js
@@ -0,0 +1,23 @@
+. . .
+
+import { Control, Form, Errors, actions } from 'react-redux-form';
+
+. . .
+
+    handleSubmit(values) {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
+        // event.preventDefault();
+    }
+
+. . .
+
+                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
+                          
+                          . . .
+                          
+
+                        </Form>
+
+. . .
